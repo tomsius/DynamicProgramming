19915798330c1ec692bcc8c516acfb0eae864a23
Tomas Kašelynas <tomaskas98@gmail.com>
2021-09-03 11:15:02 +0300

    FibonacciMemoization

---
+++ DynamicProgrammingDemo/Fibonacci/FibonacciMemoization.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    public static class FibonacciMemoization
+    {
+        public static long FibRec(int n, long[] memo = null)
+        {
+            if (memo == null)
+            {
+                memo = new long[n];
+            }
+
+            if (memo[n - 1] != 0)
+            {
+                return memo[n - 1];
+            }
+
+            if (n <= 2)
+            {
+                memo[n - 1] = 1;
+                return 1;
+            }
+
+            memo[n - 1] = FibRec(n - 1, memo) + FibRec(n - 2, memo);
+
+            return memo[n - 1];
+        }
+
+        public static long FibIterative(int n)
+        {
+            if (n <= 2)
+            {
+                return 1;
+            }
+
+            long[] memo = new long[n];
+            memo[0] = 1;
+            memo[1] = 1;
+
+            for (int i = 2; i < n; i++)
+            {
+                memo[i] = memo[i - 1] + memo[i - 2];
+            }
+
+            return memo[n - 1];
+        }
+    }
+}
---
+++ DynamicProgrammingDemo/Fibonacci/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    /*
+     * Write a function that takes in a number as an argument.
+     * The function should return the n-th number of the Fibonacci sequence.
+     */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FibRec(6));
+            Console.WriteLine(FibRec(7));
+            Console.WriteLine(FibRec(8));
+            //Console.WriteLine(FibRec(50));
+            Console.WriteLine();
+
+            Console.WriteLine(FibonacciMemoization.FibRec(6));
+            Console.WriteLine(FibonacciMemoization.FibRec(7));
+            Console.WriteLine(FibonacciMemoization.FibRec(8));
+            Console.WriteLine(FibonacciMemoization.FibRec(50));
+            Console.WriteLine();
+
+            Console.WriteLine(FibonacciMemoization.FibIterative(6));
+            Console.WriteLine(FibonacciMemoization.FibIterative(7));
+            Console.WriteLine(FibonacciMemoization.FibIterative(8));
+            Console.WriteLine(FibonacciMemoization.FibIterative(50));
+        }
+
+        public static int FibRec(int n)
+        {
+            if (n <= 2)
+            {
+                return 1;
+            }
+
+            return FibRec(n - 1) + FibRec(n - 2);
+        }
+    }
+}
